ffb266a53f253f3b0d8b53e58c3468802c88cdd3
tibby <tibby@tibbys-MacBook-Air.local>
2021-01-26 22:50:11 +0700

    Initial app

---
+++ spec/views/products/show.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "products/show", type: :view do
+  before(:each) do
+    @product = assign(:product, Product.create!(
+      name: "Name",
+      description: "MyText",
+      price: 2,
+      amount: 3
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Name/)
+    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/2/)
+    expect(rendered).to match(/3/)
+  end
+end
